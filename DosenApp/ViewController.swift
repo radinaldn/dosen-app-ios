91d34d0a7f5dee3f3785927caea6b48e624c75ff
user <user@users-MacBook-Air.local>
2018-11-05 07:36:28 +0700

    Initial Commit

---
+++ DosenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DosenApp
+//
+//  Created by Aldresti on 05/11/18.
+//  Copyright © 2018 TopApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
