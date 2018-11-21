375a74fb0e1a90d2d6561e3ad7a0eab3bca65c2c
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2018-11-21 22:33:00 +0700

    target ios 12

--- DosenApp/ViewController.swift
+++ DosenApp/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import WebKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    override func loadView() {
+        webView = WKWebView();
+        webView.navigationDelegate = self;
+        view = webView;
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //locationManager.delegate = self
+        startLocation = nil
+        
+        // 1
+        let url = URL(string: "https://topapp.id/smart-presence/web")!
+        webView.load(URLRequest(url: url))
+        
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
+    @IBAction func startWhenInUse(_sender: Any){
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func starAlways(_sender: Any){
+        locationManager.stopUpdatingLocation()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func resetDistance(_sender: Any){
+        startLocation = nil
+    }
+    
+    func locationManager(_manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]){
+        let latestLocation: CLLocation = locations[locations.count - 1]
+        
+        print(latestLocation.coordinate.latitude)
+        print(latestLocation.coordinate.longitude)
+        print(latestLocation.altitude)
+        
+        
+        if (startLocation == nil){
+            startLocation = latestLocation
+        }
+        
+        let distanceBetween: CLLocationDistance =
+        latestLocation.distance(from: startLocation)
+        
+        print(distanceBetween)
+    }
+    
+    func locationManager(_manager: CLLocationManager,
+                         didFailWithError error: Error){
+        print(error.localizedDescription)
+    }
 
 }
 
